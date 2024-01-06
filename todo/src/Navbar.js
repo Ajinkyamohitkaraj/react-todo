0dde8a0e65b0c5fcdbe12b4375b9a5672443536f
Ajinkyammohitkaraj <mohitkarap@rknec.edu>
2024-01-06 23:09:54 +0530

    initialised react project

---
+++ todo/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[50px] bg-purple-400 justify-between items-center'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
